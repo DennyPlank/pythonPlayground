2f95a5e536482cdae05d051b64b20c3e6d4ab71e
Dennis Plank <DennyMouzon@gmail.com>
2022-08-07 14:56:36 -0700

    basic python stuff

--- pythonPlayground.py
+++ pythonPlayground.py
@@ -3,10 +3,16 @@
 my_first_tupal = ("Dennis", 27, "Male")
 
 name, age, gender = my_first_tupal
-print(my_first_tupal)
-print(name)
-print(age)
-print(gender)
-print(name + " " + str(age) + " " + gender)
+# print(my_first_tupal)
+# print(name)
+# print(age)
+# print(gender)
+# print(name + " " + str(age) + " " + gender)
 
+# Playing with zips()
 
+kids = ["Darryl", "Emery", "Ellie"]
+ages = [8, 6, 4]
+
+kids_and_ages = zip(kids, ages)
+print(list(kids_and_ages))
