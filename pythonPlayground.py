9dbdfa5a88118ca46fb4dd87002d5b46135904ff
Dennis Plank <DennyMouzon@gmail.com>
2022-08-09 09:54:50 -0700

    basic python stuff

--- pythonPlayground.py
+++ pythonPlayground.py
@@ -26,12 +26,29 @@
 # for x in range(100):
 #     print(statement + " " + str(x))
 
-# Loops and if statement control flow
-dog_breeds_available_for_adoption = ["french_bulldog", "dalmatian", "shihtzu", "poodle", "collie"]
-dog_breed_I_want = "dalmatian"
+# Hairstyles
+hairstyles = ["bouffant", "pixie", "dreadlocks", "crew", "bowl", "bob", "mohawk", "flattop"]
 
-for dog_breed in dog_breeds_available_for_adoption:
-  print(dog_breed)
-  if dog_breed == dog_breed_I_want:
-    print("They have the dog I want!")
-    break;
+prices = [30, 25, 40, 20, 20, 35, 50, 35]
+
+last_week = [2, 3, 5, 8, 4, 4, 6, 2]
+
+total_price = sum(prices)
+average_price = total_price / len(prices)
+
+print("Average Haircut Price: " + str(average_price))
+
+new_prices = [price - 5 for price in prices]
+
+print(new_prices)
+
+total_revenue = 0
+for i in range(0, len(hairstyles)):
+  revenue = prices[i] * last_week[i]
+  total_revenue += revenue
+
+print("Total Revenue: " + str(total_revenue))
+average_daily_revenue = total_revenue / 7
+print(average_daily_revenue)
+
+under_30 = [price for price in prices if price < 30];
