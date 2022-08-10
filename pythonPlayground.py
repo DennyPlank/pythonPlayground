044772ddcc4d0d19d0f9ad38df3f5d9ff762497a
Dennis Plank <DennyMouzon@gmail.com>
2022-08-09 17:02:59 -0700

    python functions

--- pythonPlayground.py
+++ pythonPlayground.py
@@ -1,17 +1,30 @@
-# First functions used in python
-current_budget = 3500.75
-shirt_expense = 9
-
-def print_remaining_budget(budget):
-  print("Your remaining budget is: $" + str(budget))
-
-print_remaining_budget(current_budget)
+# Uncomment this when you reach the "Use the Force" section
+train_mass = 22680
+train_acceleration = 10
+train_distance = 100
+bomb_mass = 1
+c = 3*10**8
 
 # Write your code below: 
+def f_to_c(f):
+  return (f - 32) * (5/9)
 
-def deduct_expense(budget, expense):
-  return budget - expense
+def c_to_f(c):
+  return (c * (9/5)) + 32
 
-new_budget_after_shirt = deduct_expense(current_budget, shirt_expense)
+def get_force(mass, acc):
+  return mass * acc
 
-print_remaining_budget(new_budget_after_shirt)
+def get_energy(mass):
+  return c * mass
+
+def get_work(mass, acc, distance):
+  return get_force(mass, acc) * distance
+
+train_force = get_force(train_mass, train_acceleration)
+
+print(f" The GE train supplies {train_force} Newtons of force.")
+
+print(f"A 1kg bomb supplies {get_energy(bomb_mass)} Joules")
+
+print(get_work(train_mass, train_acceleration, train_distance))
