16d82a6870ef18af58f815a3a838688fcd94f804
Dennis Plank <DennyMouzon@gmail.com>
2022-08-10 21:57:41 -0700

    python functions

--- pythonPlayground.py
+++ pythonPlayground.py
@@ -1,30 +1,9 @@
-# Uncomment this when you reach the "Use the Force" section
-train_mass = 22680
-train_acceleration = 10
-train_distance = 100
-bomb_mass = 1
-c = 3*10**8
-
-# Write your code below
-def f_to_c(f):
-  return (f - 32) * (5/9)
-
-def c_to_f(c):
-  return (c * (9/5)) + 32
-
-def get_force(mass, acc):
-  return mass * acc
-
-def get_energy(mass):
-  return c * mass
-
-def get_work(mass, acc, distance):
-  return get_force(mass, acc) * distance
-
-train_force = get_force(train_mass, train_acceleration)
-
-print(f" The GE train supplies {train_force} Newtons of force.")
-
-print(f"A 1kg bomb supplies {get_energy(bomb_mass)} Joules")
-
-print(get_work(train_mass, train_acceleration, train_distance))
+def over_nine_thousand(lst1):
+  i = 0
+  count = 0
+  while count <= 9000:
+    count += lst1[i] 
+    i += 1
+  return count
+#Uncomment the line below when your function is done
+print(over_nine_thousand([9000, 900, 120, 5000]))
